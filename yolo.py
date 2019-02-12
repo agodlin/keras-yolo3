dd6bc6491b52aa1c77a92af1f55aa021d9f95fd3
agodlin <arkady.godlin@intel.com>
2019-02-12 16:57:47 +0200

    Add yolo pb file

--- yolo.py
+++ yolo.py
@@ -3,143 +3,54 @@
 Class definition of YOLO_v3 style detection model on image and video
 """
 
-import colorsys
-import os
 from timeit import default_timer as timer
 
 import numpy as np
 from keras import backend as K
-from keras.models import load_model
 from keras.layers import Input
-from PIL import Image, ImageFont, ImageDraw
-
-from yolo3.model import yolo_eval, yolo_body, tiny_yolo_body, mobilenet_v2_body
+from yolo3.model import yolo_eval, mobilenet_v2_body
 from yolo3.utils import letterbox_image
 import os
-from keras.utils import multi_gpu_model
 import cv2
 
-class YOLO(object):
-    _defaults = {
-        "score" : 0.5,
-        "iou" : 0.5,
-        "model_image_size" : (176, 176),
-        "gpu_num" : 1,
-        "num_outputs": 1,
-    }
 
-    @classmethod
-    def get_defaults(cls, n):
-        if n in cls._defaults:
-            return cls._defaults[n]
-        else:
-            return "Unrecognized attribute name '" + n + "'"
-
-    def __init__(self, **kwargs):
-        print(kwargs)
-        self.__dict__.update(self._defaults) # set up default values
-        self.__dict__.update(kwargs) # and update with user overrides
-        self.class_names = self._get_class()
-        self.anchors = self._get_anchors()
+class YOLO:
+    def __init__(self, model_path, anchros, num_classes, score_th, iou_th, model_input_shape):
         self.sess = K.get_session()
+        self.model_path = model_path
+        self.anchors = anchros
+        self.num_classes = num_classes
+        self.score_th = score_th
+        self.iou_th = iou_th
+        self.model_input_shape = model_input_shape
+        self.num_outputs = 1
         self.boxes, self.scores, self.classes = self.generate()
-
-    def _get_class(self):
-        classes_path = os.path.expanduser(self.classes_path)
-        with open(classes_path) as f:
-            class_names = f.readlines()
-        class_names = [c.strip() for c in class_names]
-        return class_names
-
-    def _get_anchors(self):
-        anchors_path = os.path.expanduser(self.anchors_path)
-        with open(anchors_path) as f:
-            anchors = f.readline()
-        anchors = [float(x) for x in anchors.split(',')]
-        return np.array(anchors).reshape(-1, 2)
 
     def generate(self):
         model_path = os.path.expanduser(self.model_path)
         assert model_path.endswith('.h5'), 'Keras model or weights must be a .h5 file.'
-        print('Use model path', model_path)
+
         # Load model, or construct msodel and load weights.
         num_anchors = len(self.anchors)
-        num_classes = len(self.class_names)
 
-        input_shape = (self.model_image_size[0], self.model_image_size[1], 1)
-        print(num_anchors, num_classes, input_shape)
-        self.yolo_model = mobilenet_v2_body(Input(shape=input_shape), num_anchors//self.num_outputs, num_classes)
+        input_shape = (self.model_input_shape[0], self.model_input_shape[1], 1)
+        print('Model Info', model_path, num_anchors, self.num_classes, input_shape)
+        self.yolo_model = mobilenet_v2_body(Input(shape=input_shape), num_anchors//self.num_outputs, self.num_classes)
         self.yolo_model.load_weights(self.model_path)
-
-        self.generate_color()
 
         # Generate output tensor targets for filtered bounding boxes.
         self.input_image_shape = K.placeholder(shape=(2, ))
 
         boxes, scores, classes = yolo_eval(self.yolo_model.output, self.anchors,
-                len(self.class_names), self.input_image_shape,
-                score_threshold=self.score, iou_threshold=self.iou)
+                self.num_classes, self.input_image_shape, score_threshold=self.score_th, iou_threshold=self.iou_th)
 
         return boxes, scores, classes
-
-    def generate_color(self):
-        # Generate colors for drawing bounding boxes.
-        hsv_tuples = [(x / len(self.class_names), 1., 1.)
-                      for x in range(len(self.class_names))]
-        self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
-        self.colors = list(
-            map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
-                self.colors))
-        np.random.seed(10101)  # Fixed seed for consistent colors across runs.
-        np.random.shuffle(self.colors)  # Shuffle colors to decorrelate adjacent classes.
-        np.random.seed(None)  # Reset seed to default.
-
-    def draw_image(self, image):
-        out_boxes, out_scores, out_classes = self.detect_image(image)
-        # print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
-
-        font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
-                    size=np.floor(3e-2 * image.shape[0] + 0.5).astype('int32'))
-        thickness = (image.shape[1] + image.shape[0]) // 300
-
-        for i, c in reversed(list(enumerate(out_classes))):
-            predicted_class = self.class_names[c]
-            box = out_boxes[i]
-            score = out_scores[i]
-            if score < 0.9:
-                continue
-            label = '{} {:.2f}'.format(predicted_class, score)
-            draw = ImageDraw.Draw(image)
-            label_size = draw.textsize(label, font)
-
-            top, left, bottom, right = box
-            top = max(0, np.floor(top + 0.5).astype('int32'))
-            left = max(0, np.floor(left + 0.5).astype('int32'))
-            bottom = min(image.shape[0], np.floor(bottom + 0.5).astype('int32'))
-            right = min(image.shape[1], np.floor(right + 0.5).astype('int32'))
-            # print(label, (left, top), (right, bottom))
-
-            if top - label_size[1] >= 0:
-                text_origin = np.array([left, top - label_size[1]])
-            else:
-                text_origin = np.array([left, top + 1])
-
-            # My kingdom for a good redistributable image drawing library.
-            for i in range(thickness):
-                draw.rectangle([left + i, top + i, right - i, bottom - i], outline=(0, 255, 0))
-            draw.rectangle(
-                [tuple(text_origin), tuple(text_origin + label_size)],
-                fill=(0, 255, 0))
-            draw.text(text_origin, label, fill=0, font=font)
-            del draw
-        return image
 
     def close_session(self):
         self.sess.close()
 
-
     def detect_image(self, image):
-        boxed_image = letterbox_image(image, tuple(reversed(self.model_image_size)))
+        boxed_image = letterbox_image(image, tuple(reversed(self.model_input_shape)))
 
         image_data = np.expand_dims(boxed_image, 0)  # Add batch dimension.
 
@@ -175,13 +86,7 @@ def detect_video(yolo):
             image = np.asanyarray(image.get_data())
             if len(image.shape) > 2:
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            # bounding_boxes, landmarks = face_detector.apply_mtcnn(image)
-            bounding_boxes = np.array([])
             image = yolo.detect_image(image)
-            draw = ImageDraw.Draw(image)
-            bounding_boxes = bounding_boxes.astype(np.int)
-            if len(bounding_boxes) > 0:
-                draw.rectangle([bounding_boxes[0][0], bounding_boxes[0][1], bounding_boxes[0][2], bounding_boxes[0][3]], outline=255)
             result = np.asarray(image)
             curr_time = timer()
             exec_time = curr_time - prev_time
